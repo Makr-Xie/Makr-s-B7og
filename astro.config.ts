97ec35696156d4b09369913b34c9329c3eae671c
Makr <116593190+Makr-Xie@users.noreply.github.com>
2024-06-15 21:13:15 +0800

    initial

---
+++ astro.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import UnoCSS from 'unocss/astro';
+import { THEME_CONFIG } from "./src/theme.config";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
+export default defineConfig({
+  site: THEME_CONFIG.website,
+  prefetch: true,
+  markdown: {
+    shikiConfig: {
+      theme: 'one-dark-pro',
+      langs: [],
+      wrap: true,
+    },
+  },
+  integrations: [
+    UnoCSS({
+      injectReset: true
+    }),
+    robotsTxt(),
+    sitemap(),
+    mdx()
+  ]
+});
